faae7dcac1699535a7f8246f3949e363ea48bcbc
CreayerFix (크리이어픽스) <thdbsgh3443@kakao.com>
2025-01-28 17:06:35 +0900

    test (#3)
    * Feat: 버전 2 up (#1)
    
    * Docs: md 문서 및 text 수정
    
    * Docs: packge 버전 text 수정
    
    * Docs: text 수정
    
    * Docs: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HomePageStyled } from '@/styles/pageStyled/HomePageStyled';
 export default function HomePage() {
   return (
     <HomePageStyled>
-      <div>메인 페이지 입니다. v2</div>
+      <div>메인 페이지 입니다 v1</div>
     </HomePageStyled>
   );
 }

97f723c6ebf4a4f296f95f7e0fe9b4f0f3871c55
younhoso <thdbsgh3443@kakao.com>
2025-02-26 15:53:19 +0900

    Feat: useQuery로 로직 변경

--- src/app/membershipLevel/page.tsx
+++ src/app/membershipLevel/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 
 import clsx from 'clsx';
+
+import SkeletonProfile from '@/components/SkeletonLoading/SkeletonProfile';
+import { customAxios } from '@/libs/customAxios';
 
 import { MembershipBenefit } from '../api/membership/route';
 
@@ -15,28 +19,48 @@ type User = {
 };
 
 export default function MembershipLevelPage() {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: membershipDatas,
+    isPending: membershipLoading,
+    error: membershipError,
+  } = useQuery({
+    queryKey: ['membershipLevel'],
+    queryFn: async () => {
+      const { data } = await customAxios({ baseURL: 'http://localhost:3000/' }).get<User[]>(
+        '/api/membership',
+      );
+      return {
+        membership: data,
+      };
+    },
+  });
 
-  useEffect(() => {
-    fetch('/api/membership')
-      .then(res => res.json())
-      .then(setUsers);
-  }, []);
+  if (membershipLoading) {
+    return <SkeletonProfile />;
+  }
 
   return (
     <div className={clsx('membershipLevel')}>
-      <h1>회원 등급 리스트</h1>
+      <h2>회원 등급 리스트</h2>
+      <p>
+        회원들 정보를 조회해서 사용한 지난달 액수를 기준으로 'welcome', '치빡이', 'VIP', 'VVIP'
+        <br />
+        4등급 기준으로 나눠서 현재 등급을 보여주는 로직
+      </p>
+      <br />
+
       <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            <strong>{user.name}</strong> - {user.membershipTier} (₩
-            {user.lastMonthPurchase.toLocaleString()})<br />
-            할인율: {user.benefits.discountRate}%<br />
-            무료 배송: {user.benefits.freeShipping ? '✅' : '❌'}
-            <br />
-            우선 상담: {user.benefits.prioritySupport ? '✅' : '❌'}
-          </li>
-        ))}
+        {membershipDatas &&
+          membershipDatas.membership.map(user => (
+            <li key={user.id}>
+              <strong>{user.name}</strong> - {user.membershipTier} (₩
+              {user.lastMonthPurchase.toLocaleString()})<br />
+              할인율: {user.benefits.discountRate}%<br />
+              무료 배송: {user.benefits.freeShipping ? '✅' : '❌'}
+              <br />
+              우선 상담: {user.benefits.prioritySupport ? '✅' : '❌'}
+            </li>
+          ))}
       </ul>
     </div>
   );

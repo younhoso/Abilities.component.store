4313162e30cd5a4480a434071172370a08e4b82f
younhoso <thdbsgh3443@kakao.com>
2025-03-03 21:28:23 +0900

    Modify: className 카멜케이스 으로 수정

--- src/app/[...not-found]/page.tsx
+++ src/app/[...not-found]/page.tsx
@@ -6,7 +6,7 @@ import { NotFoundPageStyled } from '@/styles/pageStyled/NotFoundPageStyled';
 
 export default function NotFoundPage() {
   return (
-    <NotFoundPageStyled className={clsx('NotFound')}>
+    <NotFoundPageStyled className={clsx('notFound')}>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>

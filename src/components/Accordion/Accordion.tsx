12a2a9b24814c0dbf539f7670fb2eed736871fe6
younhoso <thdbsgh3443@kakao.com>
2025-03-03 21:29:44 +0900

    Feat: Accordion 컴포넌트 추가

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,7 @@
+import clsx from 'clsx';
+
+import { AccordionStyled } from './styled';
+
+export default function Accordion() {
+  return <AccordionStyled className={clsx('accordion')}></AccordionStyled>;
+}

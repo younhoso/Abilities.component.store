8086a03041f6dbc6514c7cd730ff0fb73e106804
younhoso <thdbsgh3443@kakao.com>
2025-04-15 17:11:16 +0900

    Feat: AutoSizeImage props 추가

--- src/stories/shared/ui/AutoSizeImage/AutoSizeImage.stories.tsx
+++ src/stories/shared/ui/AutoSizeImage/AutoSizeImage.stories.tsx
@@ -1,9 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import ogImageV2 from 'public/og-imageV2.png';
+
 import AutoSizeImage from '.';
 
 const meta: Meta<typeof AutoSizeImage> = {
-  title: 'AutoSizeImage',
+  title: 'Shared/AutoSizeImage',
   component: AutoSizeImage,
   tags: ['autodocs'],
   parameters: {
@@ -19,5 +21,10 @@ export default meta;
 type Story = StoryObj<typeof AutoSizeImage>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    src: ogImageV2,
+    alt: '샘플 이미지',
+    width: 300,
+    height: 200,
+  },
 };
--- src/stories/shared/ui/AutoSizeImage/styled.ts
+++ src/stories/shared/ui/AutoSizeImage/styled.ts
@@ -7,5 +7,10 @@ interface AutoSizeImageProps {}
 export const AutoSizeImageStyled = styled.div<AutoSizeImageProps>`
   position: relative;
   width: 100%;
-  padding-bottom: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* 이미지가 컨테이너에 맞게 조정되도록 */
+  }
 `;

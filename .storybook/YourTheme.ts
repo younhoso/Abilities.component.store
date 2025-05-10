cc6789dbeebe9a9820caec5b6499de643021743a
younhoso <thdbsgh3443@kakao.com>
2025-05-10 23:41:29 +0900

    Seting: storybook 전체  테마 작업

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,36 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'CreateFix UI',
+  brandUrl: '/',
+  brandImage: 'logo.png',
+  brandTarget: '_self',
+
+  //
+  colorPrimary: '#3A10E5',
+  colorSecondary: '#585C6D',
+
+  // UI
+  appBg: '#ffffff',
+  appContentBg: '#ffffff',
+  appPreviewBg: '#ffffff',
+  appBorderColor: '#585C6D',
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: '#10162F',
+  textInverseColor: '#ffffff',
+
+  // Toolbar default and active colors
+  barTextColor: '#9E9E9E',
+  barSelectedColor: '#585C6D',
+  barHoverColor: '#585C6D',
+  barBg: '#ffffff',
+
+  // Form colors
+  inputBg: '#ffffff',
+  inputBorder: '#10162F',
+  inputTextColor: '#10162F',
+  inputBorderRadius: 2,
+});
